1e75976887404d7cdd30e0bc7871632f49f728d3
KT0803 <krrishtaneja8@gmail.com>
2026-04-26 11:54:18 +0530

    major files changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,52 +1,32 @@
 import axios from "axios";
 
-// Create axios instance
+// Axios instance
 const API = axios.create({
   baseURL: "/api",
 });
 
 // Get all users
 export const getUsers = async () => {
-  try {
-    const res = await API.get("/users");
-    return res.data;
-  } catch (err) {
-    console.error("Error fetching users:", err.message);
-    throw err;
-  }
+  const res = await API.get("/users");
+  return res.data;
 };
 
 // Create user
 export const createUser = async (userData) => {
-  try {
-    const res = await API.post("/users", userData);
-    return res.data;
-  } catch (err) {
-    console.error("Error creating user:", err.message);
-    throw err;
-  }
+  const res = await API.post("/users", userData);
+  return res.data;
 };
 
-// delete user
+// Delete user
 export const deleteUser = async (id) => {
-  try {
-    const res = await API.delete(⁠ /users/${id} ⁠);
-    return res.data;
-  } catch (err) {
-    console.error("Error deleting user:", err.message);
-    throw err;
-  }
+  const res = await API.delete(`/users/${id}`);
+  return res.data;
 };
 
-// update user
+// Update user
 export const updateUser = async (id, data) => {
-  try {
-    const res = await API.put(⁠ /users/${id} ⁠, data);
-    return res.data;
-  } catch (err) {
-    console.error("Error updating user:", err.message);
-    throw err;
-  }
+  const res = await API.put(`/users/${id}`, data);
+  return res.data;
 };
 
 export default API;
