1eac6e1b1a48ace155f9df3c527c66be4f087c08
KT0803 <krrishtaneja8@gmail.com>
2026-04-26 12:04:30 +0530

    major files changes

--- backend/server.js
+++ backend/server.js
@@ -13,19 +13,14 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
 app.use("/api/users", userRoutes);
-
+app.use("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../frontend/public","index.html"))
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(⁠`Server running on port ${PORT}`);
 });
